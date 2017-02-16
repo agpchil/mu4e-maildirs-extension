83a8befbe3ad998193990a76443f95a8c1f010e8
Sébastien Le Callonnec <sebastien@weblogism.com>
2017-02-16 19:53:46 +0000

    Use `mu4e-message-changed-hook` hook.
    As per @joostkremers, `mu4e-msg-changed-hook` was renamed to
    `mu4e-message-changed-hook` in mu (Cf.
    https://github.com/djcb/mu/commit/600955eebca308c6bb6887051904967f4f786832).
    This fixes #34.

--- mu4e-maildirs-extension.el
+++ mu4e-maildirs-extension.el
@@ -788,16 +788,16 @@ When preceded with `universal-argument':
 (defun mu4e-maildirs-extension-load ()
   "Initialize."
   (mu4e-maildirs-extension-unload)
-  (if (boundp 'mu4e-msg-changed-hook)
-      (add-hook 'mu4e-msg-changed-hook mu4e-maildirs-extension-index-updated-func)
+  (if (boundp 'mu4e-message-changed-hook)
+      (add-hook 'mu4e-message-changed-hook mu4e-maildirs-extension-index-updated-func)
   (add-hook 'mu4e-index-updated-hook mu4e-maildirs-extension-index-updated-func))
   (add-hook 'mu4e-main-mode-hook mu4e-maildirs-extension-main-view-func))
 
 ;;;###autoload
 (defun mu4e-maildirs-extension-unload ()
-  "Initialize."
-  (if (boundp 'mu4e-msg-changed-hook)
-      (remove-hook 'mu4e-msg-changed-hook mu4e-maildirs-extension-index-updated-func)
+  "Un-initialize."
+  (if (boundp 'mu4e-message-changed-hook)
+      (remove-hook 'mu4e-message-changed-hook mu4e-maildirs-extension-index-updated-func)
     (remove-hook 'mu4e-index-updated-hook mu4e-maildirs-extension-index-updated-func))
   (remove-hook 'mu4e-main-mode-hook mu4e-maildirs-extension-main-view-func))
 
